2ff63cbb10a967920e9535cb16c5f9abd579d07d
Einar Høst <Einar.Host@nrk.no>
2019-10-19 14:02:18 +0200

    vector tests

---
+++ Oops.Tests/VectorTests.cs
@@ -0,0 +1,77 @@
+using Xunit;
+
+namespace Oops.Tests
+{
+    public class VectorTests
+    {
+        [Fact]
+        public void AddVectors_AddingNullVector()
+        {
+            var v = new Vector(1, 2);
+            Assert.Equal(v, v + new Vector(0, 0));
+            Assert.Equal(v, new Vector(0, 0) + v);
+        }
+
+        [Fact]
+        public void AddVectors_VectorsThatAddUpToNullVector()
+        {
+            var v1 = new Vector(1, 2);
+            var v2 = new Vector(-1, -2);
+            Assert.Equal(new Vector(0, 0), v1 + v2);
+        }
+
+        [Fact]
+        public void AddVectors_AddingVectorToItselfDoublesIt()
+        {
+            var v = new Vector(1, 2);
+            Assert.Equal(new Vector(2, 4), v + v);
+        }
+
+        [Fact]
+        public void NegateVector_Example1()
+        {
+            var v = new Vector(1, 2);
+            Assert.Equal(new Vector(-1, -2), -v);
+        }
+
+        [Fact]
+        public void NegateVector_Example2()
+        {
+            var v = new Vector(3, 3);
+            Assert.Equal(new Vector(-3, -3), -v);
+        }
+
+        [Fact]
+        public void NegateVector_NullVector()
+        {
+            var v = new Vector(0, 0);
+            Assert.Equal(v, -v);
+        }
+
+
+        [Fact]
+        public void SubtractVectors_SubtractingNullVector()
+        {
+            var nv = new Vector(0, 0);
+            var v = new Vector(1, 3);
+            Assert.Equal(v, v - nv);
+        }
+
+        [Fact]
+        public void SubtractVectors_SubtractingVectorFromItselfYieldsNullVector()
+        {
+            var nv = new Vector(0, 0);
+            var v = new Vector(1, 3);
+            Assert.Equal(new Vector(0, 0), v - v);
+        }
+
+        [Fact]
+        public void SubtractVectors_Example()
+        {
+            var v1 = new Vector(3, 5);
+            var v2 = new Vector(1, 1);
+            Assert.Equal(new Vector(2, 4), v1 - v2);
+            Assert.Equal(new Vector(-2, -4), v2 - v1);
+        }
+    }
+}
--- Oops/Vector.cs
+++ Oops/Vector.cs
@@ -1,6 +1,8 @@
-﻿namespace Oops
+﻿using System;
+
+namespace Oops
 {
-    internal class Vector
+    public class Vector : IEquatable<Vector>
     {
         private readonly double _x;
         private readonly double _y;
@@ -9,6 +11,41 @@
         {
             _x = x;
             _y = y;
+        }
+
+        public double X()
+        {
+            return _x;
+        }
+
+        public double Y()
+        {
+            return _y;
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return v1.Add(v2);
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return v1.Subtract(v2);
+        }
+
+        public static Vector operator -(Vector v)
+        {
+            return v.Negate();
+        }
+
+        public static Vector operator *(Vector v, double f)
+        {
+            return v.Scale(f);
+        }
+
+        public static Vector operator *(double f, Vector v)
+        {
+            return v.Scale(f);
         }
 
         public Vector Add(Vector v)
@@ -29,6 +66,44 @@
         public Vector Scale(double factor)
         {
             return new Vector(_x * factor, _y * factor);
+        }
+
+        public override string ToString()
+        {
+            return $"Vector ({_x}, {_y})";
+        }
+
+        public bool Equals(Vector other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return _x.Equals(other._x) && _y.Equals(other._y);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Vector) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (_x.GetHashCode() * 397) ^ _y.GetHashCode();
+            }
+        }
+
+        public static bool operator ==(Vector left, Vector right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Vector left, Vector right)
+        {
+            return !Equals(left, right);
         }
     }
 }
