e63653728989c0abca7ebfeff1f19e193c062ff8
Einar Høst <Einar.Host@nrk.no>
2019-10-19 17:44:36 +0200

    Box tests for Flip and Toss.

--- Oops.Tests/BoxTests.cs
+++ Oops.Tests/BoxTests.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 
 namespace Oops.Tests
@@ -26,6 +25,29 @@ namespace Oops.Tests
         {
             var box = new Box(new Vector(1, 2), new Vector(2, 0), new Vector(0, 3));
             Assert.Equal(box, box.Turn().Turn().Turn().Turn());
+        }
+
+        [Fact]
+        public void Box_Flip()
+        {
+            var box = new Box(new Vector(1, 2), new Vector(2, 0), new Vector(0, 3));
+            var expected = new Box(new Vector(3, 2), new Vector(-2, 0), new Vector(0, 3));
+            Assert.Equal(expected, box.Flip());
+        }
+
+        [Fact]
+        public void Box_Flip_Twice()
+        {
+            var box = new Box(new Vector(1, 2), new Vector(2, 0), new Vector(0, 3));
+            Assert.Equal(box, box.Flip().Flip());
+        }
+
+        [Fact]
+        public void Box_Toss()
+        {
+            var box = new Box(new Vector(1, 2), new Vector(2, 0), new Vector(0, 2));
+            var expected = new Box(new Vector(2, 3), new Vector(1, 1), new Vector(-1, 1));
+            Assert.Equal(expected, box.Toss());
         }
     }
 }
