57439fa0ae948f8ff3760e63d693b8088056f183
Einar Høst <Einar.Host@nrk.no>
2020-02-16 10:21:18 +0100

    fluid extensions

--- Oops/Picture.cs
+++ Oops/Picture.cs
@@ -6,45 +6,5 @@ namespace Oops
     internal abstract class Picture
     {
         public abstract IReadOnlyList<IShape> Render(Box box);
-
-        public Picture Turn()
-        {
-            return new TurnedPicture(this);
-        }
-
-        public Picture Flip()
-        {
-            return new FlippedPicture(this);
-        }
-
-        public Picture Toss()
-        {
-            return new TossedPicture(this);
-        }
-
-        public Picture Above(Picture p)
-        {
-            return new AbovePicture(this, p);
-        }
-
-        public Picture Above(Picture p, double topWeight, double bottomWeight)
-        {
-            return new AbovePicture(this, p, topWeight, bottomWeight);
-        }
-
-        public Picture Beside(Picture p)
-        {
-            return new BesidePicture(this, p);
-        }
-
-        public Picture Beside(Picture p, double leftWeight, double rightWeight)
-        {
-            return new BesidePicture(this, p, leftWeight, rightWeight);
-        }
-
-        public Picture Over(Picture p)
-        {
-            return new OverPicture(this, p);
-        }
     }
 }
---
+++ Oops/PictureExtensions.cs
@@ -0,0 +1,45 @@
+﻿namespace Oops
+{
+    internal static class PictureExtensions
+    {
+        public static Picture Turn(this Picture p)
+        {
+            return new TurnedPicture(p);
+        }
+
+        public static Picture Flip(this Picture p)
+        {
+            return new FlippedPicture(p);
+        }
+
+        public static Picture Toss(this Picture p)
+        {
+            return new TossedPicture(p);
+        }
+
+        public static Picture Above(this Picture p, Picture q)
+        {
+            return new AbovePicture(p, q);
+        }
+
+        public static Picture Above(this Picture p, Picture q, double topWeight, double bottomWeight)
+        {
+            return new AbovePicture(p, q, topWeight, bottomWeight);
+        }
+
+        public static Picture Beside(this Picture p, Picture q)
+        {
+            return new BesidePicture(p, q);
+        }
+
+        public static Picture Beside(this Picture p, Picture q, double leftWeight, double rightWeight)
+        {
+            return new BesidePicture(p, q, leftWeight, rightWeight);
+        }
+
+        public static Picture Over(this Picture p, Picture q)
+        {
+            return new OverPicture(p, q);
+        }
+    }
+}
